e27016849e25eb8489a9430c4b35c7f51f0777f2
S3ki <92526584+S3ki@users.noreply.github.com>
2024-02-02 13:56:50 +0200

    Iteration 5 completed

--- src/App.js
+++ src/App.js
@@ -1,47 +1,18 @@
-<<<<<<< HEAD
 import './App.css';
 import Greetings from './components/iteration2.js'
 import BoxColor from './components/iteration4.js';
 import CreditCard from './components/iteration5.js'
-=======
 import React from 'react';
 import IdCard from './components/IdCard';
 import './App.css';
 import Random from './components/Random';
->>>>>>> main
+
 
 function App() {
   return (
     <div className="App">
-<<<<<<< HEAD
-      <h2>iteration2</h2>
-      <p>
-        <Greetings lang="de">Ludwig</Greetings>
-        <Greetings lang="fr">François</Greetings>
-      </p>
-      <h3>iteration4</h3>
-        <p>
-          <div className='box'>
-            <BoxColor r={255} g={0} b={0} />
-            <BoxColor r={128} g={255} b={0} />
-          </div>
-        </p>
-      <h5>iteration5</h5>
-      <p>
-        <div className='kortti'>
-          <CreditCard
-            type="Visa"
-            number="0123456789018845"
-            expirationMonth={3}
-            expirationYear={2021}
-            bank="BNP"
-            owner="Maxence Bouret"
-            bgColor="#11aa99"
-            color="white" 
-          />
-        </div>
-      </p>
-=======
+
+
       <h1>Iteration 1</h1>
   <div className='centerbox'>
 
@@ -62,13 +33,64 @@ function App() {
       birth={new Date("1988-05-11")}
       picture="https://randomuser.me/api/portraits/women/44.jpg"
       />
+
+      <h2>iteration2</h2>
+      <p>
+        <Greetings lang="de">Ludwig</Greetings>
+        <Greetings lang="fr">François</Greetings>
+      </p>
+
       <h1>Iteration 3</h1>
       <Random min={1} max={6}/>
       <Random min={1} max={100}/>
     
+      <h3>iteration4</h3>
+        <p>
+          <div className='box'>
+            <BoxColor r={255} g={0} b={0} />
+            <BoxColor r={128} g={255} b={0} />
+          </div>
+        </p>
+      <h5>iteration5</h5>
+      <p>
+        <div className='kortti'>
+          <CreditCard
+            type="Visa"
+            number="0123456789018845"
+            expirationMonth={3}
+            expirationYear={2021}
+            bank="BNP"
+            owner="Maxence Bouret"
+            bgColor="#11aa99"
+            color="white" 
+          />
+            <CreditCard
+              type="Master Card"
+              number="0123456789010995"
+              expirationMonth={3}
+              expirationYear={2021}
+              bank="N26"
+              owner="Maxence Bouret"
+              bgColor="#eeeeee"
+              color="#222222"
+            />
+                
+            <CreditCard
+              type="Visa"
+              number="0123456789016984"
+              expirationMonth={12}
+              expirationYear={2019}
+              bank="Name of the Bank"
+              owner="Firstname Lastname"
+              bgColor="#ddbb55"
+              color="white" 
+            />
+        </div>
+        
+      </p>
       </div>
 
->>>>>>> main
+
     </div>
   );
 }
--- src/components/iteration5.js
+++ src/components/iteration5.js
@@ -1,4 +1,5 @@
-
+import Visa from './pics/visa.png'
+import Master from './pics/mastercard.png'
 const Creditcard = ({type, number, expirationMonth, expirationYear, bank, owner, bgColor, color}) => {
     const cardStyle = {
         backgroundColor: bgColor,
@@ -7,28 +8,42 @@ const Creditcard = ({type, number, expirationMonth, expirationYear, bank, owner,
         display: 'flex',
         flexDirection: 'column',
         width: '300px',
+        gap: '10px',
+       
     };
     
     const cardTypeStyle = {
-        width: '40px',
+        width: '500px',
+
       };
+const cardNames = {
+      display: 'flex',
+      marginLeft: '10px'
+}
+const cardName = {
+      display: 'flex',
+      marginLeft: '10px',
+      marginBottom: '10px'
+   
+}
 
     return (
         <div style={cardStyle}>
           <div style={cardTypeStyle}>
             {type === 'Visa' ? (
-              <span>Visa</span>
+              <img style={{ width: '50px', margin: ''}} src={Visa} alt={`kuva`}></img>
             ) : (
-              <span>Mastercard</span>
+              <span><img style={{ width: '50px'}} src={Master} alt={`kuva`}></img></span>
             )}
           </div>
-          <div>{'●●●● ' + number.slice(-4)}</div>
-          <div>
+          <div>{'●●●●  ●●●●  ●●●● ' + number.slice(-4)}</div>
+          <div style={cardNames}>
             <span>Expires {expirationMonth}/{expirationYear}</span>
             <span>{bank}</span>
           </div>
-          <div>{owner}</div>
-        </div>
+          <div style={cardName}>{owner}</div>
+          </div>
+       
       );
 };
 
